b94d1cfbf564c529c79fd3156d5dbc0b0d5a192f
GorodetskiyKP <gorodetskiykp@gmail.com>
2024-01-16 09:59:03 +0300

    Добавил кнопки региона и состава семьи

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import logging
 
+from collections import defaultdict
+
 import telebot
 
 from telebot import types
 
+import buttons as b
 import messages as m
 
-from buttons import regions
 from config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
@@ -14,28 +16,50 @@ bot = telebot.TeleBot(TOKEN)
 logger = telebot.logger
 logger.setLevel(logging.DEBUG)
 
-user_info = {}
+user_info = defaultdict(dict)
 
 
 @bot.callback_query_handler(
     func=lambda call: 'region_button_pressed' in call.data)
 def region_button_pressed(call):
-    print(call)
     _, region = call.data.split(':')
-    user_info['region'] = region
-    bot.send_message(call.message.chat.id, str(user_info))
+    region_title = sorted(b.regions)[int(region)]
+    user_info[call.message.chat.id]['region'] = region_title
+    user_info[call.message.chat.id]['money'] = b.regions[region_title]
+    get_family_info(call.message)
 
 
 def get_location(message):
     keyboard = types.InlineKeyboardMarkup(row_width=1)
-    for region in regions:
+    for idx, region in enumerate(sorted(b.regions)):
         keyboard.add(
             types.InlineKeyboardButton(
                 text=region,
-                callback_data='region_button_pressed:{}'.format(region),
+                callback_data='region_button_pressed:{}'.format(idx),
             )
         )
     bot.send_message(message.chat.id, m.location, reply_markup=keyboard)
+
+
+@bot.callback_query_handler(
+    func=lambda call: 'family_info_button_pressed' in call.data)
+def family_info_button_pressed(call):
+    _, info = call.data.split(':')
+    info_title = sorted(b.family_info)[int(info)]
+    user_info[call.message.chat.id]['family_info'] = info_title
+    bot.send_message(call.message.chat.id, str(user_info))
+
+
+def get_family_info(message):
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
+    for idx, info in enumerate(sorted(b.family_info)):
+        keyboard.add(
+            types.InlineKeyboardButton(
+                text=info,
+                callback_data='family_info_button_pressed:{}'.format(idx),
+            )
+        )
+    bot.send_message(message.chat.id, m.family_info, reply_markup=keyboard)
 
 
 def get_age(message):
@@ -45,7 +69,10 @@ def get_age(message):
 
 def save_age(message):
     try:
-        user_info['age'] = int(message.text)
+        age = int(message.text)
+        user_info[message.chat.id]['age'] = age
+        if age <= 25:
+            bot.send_message(message.chat.id, m.age25)
     except ValueError:
         bot.send_message(message.chat.id, m.age_error)
         get_age(message)
@@ -53,7 +80,7 @@ def save_age(message):
 
 
 def get_user_name(message):
-    user_info['name'] = message.text.title()
+    user_info[message.chat.id]['name'] = message.text.title()
     get_age(message)
 
 
@@ -64,4 +91,4 @@ def start_message(message):
     bot.register_next_step_handler(message, get_user_name)
 
 
-bot.infinity_polling(none_stop=True)
+bot.infinity_polling()
